c35ff8f6437ddf4fcf548beba6e11e1f5044ed95
muhammadhammadahmed03122169468 <hammad03122169468@gmail.com>
2024-02-04 15:37:54 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+
+// chapter 1 
+
+alert("Error! Enter a valid password.")
+
+
+alert("Welcome to js land\nHappy coading")
+
+alert("Welcome to js land")
+
+alert("Hello.. i can run JS through my web browser's console" )
+
+alert("Happy Coading\nPrevent this page from creating additional dailogs")
